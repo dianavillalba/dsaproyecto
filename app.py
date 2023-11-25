5c7d108f2f739a036102e674ab9d09fbe7461733
dianavillalba <93057437+dianavillalba@users.noreply.github.com>
2023-11-25 12:55:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def generar_playlist():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
